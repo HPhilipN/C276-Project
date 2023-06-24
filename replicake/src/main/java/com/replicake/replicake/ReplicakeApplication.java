80e2ca4722149dac5c850783868d6d93ac958223
Sean Lam <orange75100@gmail.com>
2023-06-23 17:18:50 -0700

    spring boot initialized :>

---
+++ replicake/src/main/java/com/replicake/replicake/ReplicakeApplication.java
@@ -0,0 +1,13 @@
+package com.replicake.replicake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicakeApplication.class, args);
+	}
+
+}
---
+++ replicake/src/test/java/com/replicake/replicake/ReplicakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replicake.replicake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
