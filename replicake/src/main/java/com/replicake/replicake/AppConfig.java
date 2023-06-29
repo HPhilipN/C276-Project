f6cd8fe39bd371431cbea2aabac3b8d5418c467f
Jai <malhi.jaivir@gmail.com>
2023-06-29 13:30:36 -0700

    moved environment variables to .env, located in root

---
+++ replicake/src/main/java/com/replicake/replicake/AppConfig.java
@@ -0,0 +1,36 @@
+package com.replicake.replicake;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySource("file:${user.dir}/.env")
+public class AppConfig {
+    
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(datasourceUrl);
+        dataSource.setUsername(datasourceUsername);
+        dataSource.setPassword(datasourcePassword);
+        // Additional configuration for your data source if needed
+        return dataSource;
+    }
+
+    // Other configuration beans or application-specific configurations...
+
+}
--- replicake/src/main/java/com/replicake/replicake/ReplicakeApplication.java
+++ replicake/src/main/java/com/replicake/replicake/ReplicakeApplication.java
@@ -3,27 +3,11 @@ package com.replicake.replicake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class ReplicakeApplication {
-    static String db_url, db_username, db_password;
 
     public static void main(String[] args) {
-        envVariables();
         SpringApplication.run(ReplicakeApplication.class, args);
     }
 
-    private static void envVariables(){
-        Dotenv dotenv = Dotenv.load();
-
-        // Access environment variables
-        db_url = dotenv.get("DATABASE_URL");
-        db_username = dotenv.get("DATABASE_USERNAME");
-        db_password = dotenv.get("DATABASE_PASSWORD");
-
-        System.out.println(db_url);
-        System.out.println(db_username);
-        System.out.println(db_password);
-    }
 }
