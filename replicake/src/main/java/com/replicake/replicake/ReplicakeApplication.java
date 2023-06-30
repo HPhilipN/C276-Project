e9d134522c1f02e0615a94bfc5441da17008c860
Jai <malhi.jaivir@gmail.com>
2023-06-30 15:39:44 -0700

    correcting env variables

--- replicake/src/main/java/com/replicake/replicake/ReplicakeApplication.java
+++ replicake/src/main/java/com/replicake/replicake/ReplicakeApplication.java
@@ -3,11 +3,20 @@ package com.replicake.replicake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ReplicakeApplication {
 
     public static void main(String[] args) {
+        loadEnvVariables();
         SpringApplication.run(ReplicakeApplication.class, args);
     }
 
+    private static void loadEnvVariables() {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+    }
 }
