91b093c7f29f4d43fb00854f68cd1bf49c345acb
Jai <malhi.jaivir@gmail.com>
2023-06-30 15:39:57 -0700

    correcting env variables

--- replicake/src/main/java/com/replicake/replicake/AppConfig.java
+++ replicake/src/main/java/com/replicake/replicake/AppConfig.java
@@ -5,7 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+// import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
