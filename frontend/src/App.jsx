45e5541283a7535752444ba6235b00ebd536ae2c
Yuzu-sorbet <126542378+Yuzu-sorbet@users.noreply.github.com>
2023-07-02 19:50:10 -0700

    added structure for searchbar, filter and recipe list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import "./Login.css"
 import Home from "./Home.jsx";
 import Login from "./Login.jsx"
-import Searchbar from "./Searchbar";
+import Dashboard from "./dashboard";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />} />
-          <Route path="/search" element ={<Searchbar/>} ></Route>
+          <Route path="/dashboard" element={<Dashboard />}/>
         </Routes>
     </Router>
     </div>
