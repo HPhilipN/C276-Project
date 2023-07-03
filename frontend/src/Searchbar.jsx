a234ed32fa83bf45ec15da21299ab3e9c9472291
Yuzu-sorbet <126542378+Yuzu-sorbet@users.noreply.github.com>
2023-07-02 22:25:34 -0700

    added search bar non functional

--- frontend/src/Searchbar.jsx
+++ frontend/src/Searchbar.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import React from "react";
 import "./Searchbar.css";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faFilter } from '@fortawesome/free-solid-svg-icons';
 
-const Searchbar = () => {
-    return (
-        <div>
-            <h1>Search Bar</h1>
+const Searchbar = ({value, changeInput}) => (
+
+    
+    <div class="searchbar-wrap">
+        <div class="searchbar-icon">
+            <FontAwesomeIcon icon={faMagnifyingGlass} size="10px"/>
         </div>
-    )
-}
+        <input type="text" placeholder="Search Here" value={value} onChange={changeInput}/>
+    </div>
+)
+
 export default Searchbar
