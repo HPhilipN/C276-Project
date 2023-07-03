cdf056b110402d4cdd2d2fdda2f85915938270af
Yuzu-sorbet <126542378+Yuzu-sorbet@users.noreply.github.com>
2023-07-02 18:46:44 -0700

    added searchbar no css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import "./Login.css"
 import Home from "./Home.jsx";
 import Login from "./Login.jsx"
+import Searchbar from "./Searchbar";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />} />
+          <Route path="/search" element ={<Searchbar/>} ></Route>
         </Routes>
     </Router>
     </div>
