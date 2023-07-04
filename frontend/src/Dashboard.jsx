ff15196eb6e8f4a13110108c878d9888768630ee
Yuzu-sorbet <126542378+Yuzu-sorbet@users.noreply.github.com>
2023-07-03 19:04:32 -0700

    added link to dashboard from search for recipes button

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Filter from "./filter";
 import Searchbar from "./Searchbar";
 import Recipelist from "./Recipelist";
-import Navbar from "./NavbarLogin";
+import Navbar from "./Navbar";
 import "./Dashboard.css";
 
 const Dashboard = () => {
