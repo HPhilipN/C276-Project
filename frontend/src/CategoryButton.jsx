dec39ea717089849540ec78633bb5915a404f2cc
Sean Lam <orange75100@gmail.com>
2023-07-04 01:38:35 -0700

    added toggle features

--- frontend/src/CategoryButton.jsx
+++ frontend/src/CategoryButton.jsx
@@ -2,9 +2,17 @@ import React, { useEffect, useState } from "react";
 import "./styles/CategoryButton.css";
 
 export default function CategoryButton({text}) {
+    const [filterIndex, setFilterIndex] = useState(0);
+    
+    const toggleFilter = () => {
+        setFilterIndex((filterIndex + 1) % 3);
+    };
+    const filters = ["", "include", "exclude"];
     return (
-        <button key={text} className="category-button">
-            {text}
-        </button>
+        <button
+            onClick={toggleFilter}
+            key={text}
+            className={`category-button ${filters[filterIndex]}`}
+        >{text}</button>
     )
 }
