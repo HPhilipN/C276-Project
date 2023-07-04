17f05b7c0b2a7b61a8b979207819774385016d25
Sean Lam <orange75100@gmail.com>
2023-07-04 00:26:03 -0700

    use better filter categories

--- frontend/src/Filterlist.jsx
+++ frontend/src/Filterlist.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { categoryList } from "./Filtercategories";
+import categoryListJson from "./Filtercategories.json";
 import "./styles/Filterlist.css";
 
 export default function Filterlist() {
-   const itemList = categoryList.map((item) => (
-      <div className="row">
-         <h4>{item.category}</h4>
-         <button className="category-button">
-            {item.difficulty}
-            {item.type}
-            {item.occasion}
-         </button>
-      </div>
-   ));
+   // const itemList = categoryList.map((item) => (
+   //    <div className="row">
+   //       <h4>{item.category}</h4>
+   //       <button className="category-button">
+   //          {item.difficulty}
+   //          {item.type}
+   //          {item.occasion}
+   //       </button>
+   //    </div>
+   // ));
+   const categoryList = Object.keys(categoryListJson)
+   const itemList = categoryList.map((category) => (
+    <div key={category}>
+      <h4>{category}</h4>
+      {categoryListJson[category].map((categoryItem) => (
+        <button key={categoryItem} className="category-button">
+          {categoryItem}
+        </button>
+      ))}
+    </div>
+  ));
    return <div>{itemList}</div>;
 }
