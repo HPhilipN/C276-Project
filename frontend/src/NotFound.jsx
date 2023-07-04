851e646b783e582e530c6616c76c5010b0b6a680
Jai <malhi.jaivir@gmail.com>
2023-07-03 19:31:43 -0700

    added back removed styles from Filter.css

--- frontend/src/NotFound.jsx
+++ frontend/src/NotFound.jsx
@@ -1,13 +1,22 @@
+import React from "react";
+import { Box, Button, Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
+import Logo from "./assets/logo-625px.png";
+import "./styles/NotFound.css";
 
 export default function NotFound() {
    return (
-      <div>
-         <h1>Oops! You seem to be lost</h1>
-         <p>Here are some helpful links:</p>
-         <Link to="/">Home</Link>
-         <Link to="/login">Login</Link>
-         <Link to="/dashboard">Dashboard</Link>
+      <div className="container">
+         <div className="grid">
+            <h1>The page you're looking for does not exist</h1>
+            <div>
+               <p>Here are some helpful links:</p>
+               <Link to="/">Home</Link>
+               <Link to="/login">Login</Link>
+               <Link to="/dashboard">Dashboard</Link>
+            </div>
+         </div>
       </div>
    );
 }
