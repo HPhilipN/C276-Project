da1df222ca976eb593448e4dd603a4df5c691478
Phillip Nguyen <73822780+HPhilipN@users.noreply.github.com>
2023-07-04 18:03:58 -0700

    Revert "set min password length to 6"
    This reverts commit f6a31324fa86acf3bd01f6f8c4b853320cbcddb0.

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -32,19 +32,18 @@ function App() {
    };
 
    async function signUpUser(event) {
-      event.preventDefault(); // prevent page refresh on sign-up, not needed for prod
+      // event.preventDefault(); // prevent page refresh on sign-up, not needed for prod
       createUserObjectFromInputs();
-      if (newUser.password.length() >=6) {
-         console.log(newUser); //new user details
-         // "https://replicake.onrender.com/users/signup"
-         // "/users/signup"
-         fetch("https://replicake.onrender.com/users/signup", {
-            method: "POST",
-            headers: {
-               "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newUser),
-         })
+      console.log(newUser); //new user details
+      // "https://replicake.onrender.com/users/signup"
+      // "/users/signup"
+      fetch("https://replicake.onrender.com/users/signup", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify(newUser),
+      })
          .then((response) => {
             signInStatus = response.data; //TODO: get boolean return value doesnt work
             console.log(`Returned value: ${signInStatus} from /users/signup`);
@@ -53,7 +52,6 @@ function App() {
             console.log("===== ERROR =====");
             console.log(error);
          });
-      }
    }
 
    function createUserObjectFromInputs() {
