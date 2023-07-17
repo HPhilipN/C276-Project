d08ad69656a80eeb8caeaec7c9d08267b24ad2d4
Jai <malhi.jaivir@gmail.com>
2023-07-17 16:54:08 -0700

    added info button to cookbook to explain recipe display

--- frontend/src/Cookbook.jsx
+++ frontend/src/Cookbook.jsx
@@ -9,6 +9,7 @@ import NavbarAdmin from "./NavbarAdmin";
 import NavbarLogin from "./NavbarLogin";
 import "./styles/Recipes.css";
 import { UserContext } from "./utils/UserContext";
+import InfoButton from "./utils/InfoButton";
 
 // User generated recipes
 const Cookbook = () => {
@@ -82,6 +83,7 @@ const Cookbook = () => {
                <RecipeList recipes={userRecipes} />
             ) : null}
             {/* Info Button on Bottom Right */}
+            <InfoButton />
          </div>
       </div>
    );
---
+++ frontend/src/utils/InfoButton.jsx
@@ -0,0 +1,51 @@
+// Infobutton.jsx, click to show info on how recipes are displayed
+import React, { useState, useEffect, useRef } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import InfoIcon from "@mui/icons-material/Info";
+import recipeModalImg from "../assets/recipe-display.png";
+import "../styles/InfoButton.css";
+
+// User generated recipes
+const InfoButton = () => {
+   const [showModal, setShowModal] = useState(false);
+   const modalRef = useRef(null);
+   // if click outside modal, close modal
+   useEffect(() => {
+      const handleClickOutside = (event) => {
+         if (modalRef.current && !modalRef.current.contains(event.target)) {
+            setShowModal(false);
+         }
+      };
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+         document.removeEventListener("mousedown", handleClickOutside);
+      };
+   }, []);
+
+   function handleInfoButtonClick(event) {
+      event.preventDefault(); // prevent page refresh
+      setShowModal(!showModal);
+      console.log("Add Info Button clicked");
+   }
+
+   const infoModal = (
+      <div ref={modalRef} className="info-modal">
+         <button className="close-btn-parent">
+            <CloseIcon className="close-btn" onClick={handleInfoButtonClick} fontSize="large" />
+         </button>
+         <img src={recipeModalImg} />
+      </div>
+   );
+
+   return (
+      <div>
+         <button className="info-btn" onClick={handleInfoButtonClick}>
+            <InfoIcon sx={{ fontSize: 60 }} className="info-btn-icon" />
+         </button>
+         {/* {infoModal} */}
+         {showModal && infoModal}
+      </div>
+   );
+};
+
+export default InfoButton;
