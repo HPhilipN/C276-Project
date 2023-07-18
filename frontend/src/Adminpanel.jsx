98ce0049c78e1faf9a6fa2762fa1f34ae17a5e93
Yuzu-sorbet <126542378+Yuzu-sorbet@users.noreply.github.com>
2023-07-17 22:01:58 -0700

    added styling to admin panel home

--- frontend/src/Adminpanel.jsx
+++ frontend/src/Adminpanel.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import NavbarAdmin from "./NavbarAdmin";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faKitchenSet } from "@fortawesome/free-solid-svg-icons";
+import { faCookieBite } from "@fortawesome/free-solid-svg-icons";
+import "./styles/Adminpanel.css";
 
 export default function Adminhome(){
     return (
@@ -10,17 +11,20 @@ export default function Adminhome(){
             <div>
                 <NavbarAdmin/>
             </div>
-            <div>
+            <div class="innercontent">
+                <h1>Admin Panel</h1>
+                <p>Management of users and user-created recipes</p>
                 <a href="/adminusers">
+                    
                     <button className="secondary-button">
                         Manage Users
                         <FontAwesomeIcon icon={faUser} />
                     </button>
                 </a>
                 <a href="/adminrecipes">
-                    <button className="secondary-button">
+                    <button className="primary-button">
                         Manage Recipes
-                        <FontAwesomeIcon icon={faKitchenSet} />
+                        <FontAwesomeIcon icon={faCookieBite} size="lg" />
                     </button>
                 </a>
             </div>
