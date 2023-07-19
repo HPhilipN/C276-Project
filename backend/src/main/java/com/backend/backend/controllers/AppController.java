91ba791e3c5a83cc78467cfc0e63bcd21fce438c
Jai <malhi.jaivir@gmail.com>
2023-07-19 00:33:41 -0700

    prod .env location

--- backend/src/main/java/com/backend/backend/controllers/AppController.java
+++ backend/src/main/java/com/backend/backend/controllers/AppController.java
@@ -2,13 +2,19 @@ package com.backend.backend.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
-    
-    @GetMapping("{_:^(?!index\\.html$).*$}")
+
+    @GetMapping("/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/index.html";
     }
-}
 
+    @GetMapping({ "/", "/login", "/settings", "/recipes", "/cookbook/**", "/admin/**" })
+    public String forward() {
+        return "forward:/index.html";
+    }
+
+}
