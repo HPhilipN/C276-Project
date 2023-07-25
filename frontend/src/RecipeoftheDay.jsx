81cc32c7db1a3a59dfbd8a3087ccd4358779f718
Lepre-CHAU-n <71118130+Lepre-CHAU-n@users.noreply.github.com>
2023-07-24 17:53:58 -0700

    wip recipe of the day

---
+++ frontend/src/RecipeoftheDay.jsx
@@ -0,0 +1,26 @@
+// RecipeoftheDay.js
+import React from "react";
+import "./styles/RecipeoftheDay.css"; // Import the CSS file
+
+
+const RecipeoftheDay = () => {
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <h1 className="primary-heading">Recipe of the Day</h1>
+      </div>
+      <div className="testimonial-section-bottom">
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default RecipeoftheDay;
