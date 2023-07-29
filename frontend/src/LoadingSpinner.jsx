77e27e11b8a39380273b420595ea6dc376f423e3
Lepre-CHAU-n <71118130+Lepre-CHAU-n@users.noreply.github.com>
2023-07-29 16:09:45 -0700

    Revert "fixed error message"

---
+++ frontend/src/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+// LoadingSpinner.js
+import React from "react";
+import "./styles/LoadingSpinner.css";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
